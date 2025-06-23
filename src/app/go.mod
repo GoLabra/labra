fb366a6e99806f7304829be04314d7596a1eb904
crembel <sintamarian.david@gmail.com>
2025-06-23 22:45:10 +0300

    fix admin service context value

--- src/app/go.mod
+++ src/app/go.mod
@@ -11,22 +11,17 @@ require (
 	entgo.io/ent v0.14.4
 	github.com/99designs/gqlgen v0.17.73
 	github.com/GoLabra/labra/src/api v0.0.0-00010101000000-000000000000
-	github.com/caarlos0/env/v6 v6.10.1
 	github.com/centrifugal/gocent/v3 v3.3.0
 	github.com/go-chi/chi/v5 v5.2.1
 	github.com/go-chi/jwtauth/v5 v5.3.3
-	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/gorilla/websocket v1.5.3
 	github.com/hashicorp/go-multierror v1.1.1
 	github.com/iancoleman/strcase v0.3.0
-	github.com/joho/godotenv v1.5.1
-	github.com/lestrrat-go/jwx/v2 v2.1.6
 	github.com/lib/pq v1.10.9
 	github.com/lucsky/cuid v1.2.1
 	github.com/mitchellh/mapstructure v1.5.0
 	github.com/rs/cors v1.11.1
 	github.com/vektah/gqlparser/v2 v2.5.27
-	golang.org/x/crypto v0.38.0
 )
 
 require (
@@ -36,30 +31,36 @@ require (
 	github.com/agnivade/levenshtein v1.2.1 // indirect
 	github.com/apparentlymart/go-textseg/v15 v15.0.0 // indirect
 	github.com/bmatcuk/doublestar v1.3.4 // indirect
+	github.com/caarlos0/env/v6 v6.10.1 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.4.0 // indirect
 	github.com/gertd/go-pluralize v0.2.1 // indirect
 	github.com/go-openapi/inflect v0.21.2 // indirect
 	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
 	github.com/goccy/go-json v0.10.5 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/google/go-cmp v0.7.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
 	github.com/hashicorp/hcl/v2 v2.23.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/lestrrat-go/blackmagic v1.0.3 // indirect
 	github.com/lestrrat-go/httpcc v1.0.1 // indirect
 	github.com/lestrrat-go/httprc v1.0.6 // indirect
 	github.com/lestrrat-go/iter v1.0.2 // indirect
+	github.com/lestrrat-go/jwx/v2 v2.1.6 // indirect
 	github.com/lestrrat-go/option v1.0.1 // indirect
 	github.com/mitchellh/go-wordwrap v1.0.1 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	github.com/samborkent/uuidv7 v0.0.0-20231110121620-f2e19d87e48b // indirect
 	github.com/segmentio/asm v1.2.0 // indirect
 	github.com/sosodev/duration v1.3.1 // indirect
 	github.com/vmihailenco/msgpack/v5 v5.4.1 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
 	github.com/zclconf/go-cty v1.16.3 // indirect
 	github.com/zclconf/go-cty-yaml v1.1.0 // indirect
+	golang.org/x/crypto v0.38.0 // indirect
 	golang.org/x/exp v0.0.0-20250506013437-ce4c2cf36ca6 // indirect
 	golang.org/x/mod v0.24.0 // indirect
 	golang.org/x/sync v0.14.0 // indirect
--- src/app/schema/_admin/file.go
+++ src/app/schema/_admin/file.go
@@ -1,16 +1,18 @@
 package schema
 
 import (
+	"time"
+
 	"entgo.io/contrib/entgql"
 	"entgo.io/ent"
+	"entgo.io/ent/dialect"
+	"entgo.io/ent/dialect/entsql"
 	"entgo.io/ent/schema"
 	"entgo.io/ent/schema/edge"
 	"entgo.io/ent/schema/field"
-	"github.com/lucsky/cuid"
-    "entgo.io/ent/dialect"
-    "github.com/GoLabra/labra/src/api/entgql/date"
-    "time"
-    
+	"github.com/GoLabra/labra/src/api/entgql/date"
+	"github.com/GoLabra/labra/src/api/utils"
+
 	"github.com/GoLabra/labra/src/api/entgql/annotations"
 	"github.com/GoLabra/labra/src/api/entgql/entity"
 )
@@ -26,9 +28,9 @@ func (File) Annotations() []schema.Annotation {
 		entgql.RelayConnection(),
 		entgql.Mutations(entgql.MutationCreate(), entgql.MutationUpdate()),
 		annotations.Entity{
-			Caption: "File",
-			Owner:   entity.EntityOwnerAdmin,
-            DisplayField: "id",
+			Caption:      "File",
+			Owner:        entity.EntityOwnerAdmin,
+			DisplayField: "id",
 		},
 	}
 }
@@ -36,72 +38,86 @@ func (File) Annotations() []schema.Annotation {
 // Fields of the  File.
 func (File) Fields() []ent.Field {
 	return []ent.Field{
-            
-		field.String("id").DefaultFunc(cuid.New).Annotations(
+		field.String("id").DefaultFunc(utils.NewUUIDV7).Annotations(
 			entgql.OrderField("id"),
 			annotations.Field{
-				Caption:   "Id",
-				Type: entity.FieldTypeID,
+				Caption: "Id",
+				Type:    entity.FieldTypeID,
 			},
 		),
-            
-        field.Time("created_at").
-            Optional().
-            Nillable().
-            Nillable().
-            Default(func(val string) time.Time {
-                if val == "now()" {
-                    return time.Now()
-                }
-                v, err := time.Parse(date.DateTimeFormat, val)
-                if err != nil {
-                    return time.Now()
-                }
-                return v
-            }("now()")).
-            SchemaType(map[string]string{
-                dialect.MySQL:    "datetime",
-                dialect.Postgres: "timestamp",
-            }).
-            Annotations(
-                entgql.Type("DateTime"),    
+
+		field.Time("created_at").
+			Optional().
+			Nillable().
+			Nillable().
+			Default(func(val string) time.Time {
+				if val == "now()" {
+					return time.Now()
+				}
+				v, err := time.Parse(date.DateTimeFormat, val)
+				if err != nil {
+					return time.Now()
+				}
+				return v
+			}("now()")).
+			SchemaType(map[string]string{
+				dialect.MySQL:    "datetime",
+				dialect.Postgres: "timestamp",
+			}).
+			Annotations(
+				entgql.Type("DateTime"),
 				entgql.OrderField("createdAt"),
-                annotations.Field{
-                    Caption:   "Created At",
-                    Type: entity.FieldTypeDateTime,
-                    DefaultValue: "now()",
-                },
-            ),
-            
-        field.Time("updated_at").
-            Optional().
-            Nillable().
-            Nillable().
-            Default(func(val string) time.Time {
-                if val == "now()" {
-                    return time.Now()
-                }
-                v, err := time.Parse(date.DateTimeFormat, val)
-                if err != nil {
-                    return time.Now()
-                }
-                return v
-            }("now()")).
-            UpdateDefault(time.Now).
-            SchemaType(map[string]string{
-                dialect.MySQL:    "datetime",
-                dialect.Postgres: "timestamp",
-            }).
-            Annotations(
-                entgql.Type("DateTime"),    
+				annotations.Field{
+					Caption:      "Created At",
+					Type:         entity.FieldTypeDateTime,
+					DefaultValue: "now()",
+				},
+			),
+
+		field.Time("updated_at").
+			Optional().
+			Nillable().
+			Nillable().
+			Default(func(val string) time.Time {
+				if val == "now()" {
+					return time.Now()
+				}
+				v, err := time.Parse(date.DateTimeFormat, val)
+				if err != nil {
+					return time.Now()
+				}
+				return v
+			}("now()")).
+			UpdateDefault(time.Now).
+			SchemaType(map[string]string{
+				dialect.MySQL:    "datetime",
+				dialect.Postgres: "timestamp",
+			}).
+			Annotations(
+				entgql.Type("DateTime"),
 				entgql.OrderField("updatedAt"),
-                annotations.Field{
-                    Caption:   "Updated At",
-                    Type: entity.FieldTypeDateTime,
-                    DefaultValue: "now()",
-                },
-            ),
-            
+				annotations.Field{
+					Caption:      "Updated At",
+					Type:         entity.FieldTypeDateTime,
+					DefaultValue: "now()",
+				},
+			),
+
+		field.String("caption").
+			Optional().
+			Unique().
+			SchemaType(map[string]string{
+				dialect.MySQL:    "VARCHAR(255)",
+				dialect.Postgres: "VARCHAR(255)",
+			}).
+			Annotations(
+				entgql.OrderField("caption"),
+				annotations.Field{
+					Caption: "Caption",
+					Type:    entity.FieldTypeShortText,
+				},
+			),
+
 		field.String("name").
 			Unique().
 			SchemaType(map[string]string{
@@ -111,21 +127,46 @@ func (File) Fields() []ent.Field {
 			Annotations(
 				entgql.OrderField("name"),
 				annotations.Field{
-					Caption:   "Name",
-					Type: entity.FieldTypeShortText,
+					Caption: "Name",
+					Type:    entity.FieldTypeShortText,
 				},
 			),
-            
+
+		field.String("storage_file_name").
+			Unique().
+			SchemaType(map[string]string{
+				dialect.MySQL:    "VARCHAR(255)",
+				dialect.Postgres: "VARCHAR(255)",
+			}).
+			Annotations(
+				entgql.OrderField("storageFileName"),
+				entgql.Skip(entgql.SkipMutationCreateInput, entgql.SkipMutationUpdateInput),
+				annotations.Field{
+					Caption: "Storage File Name",
+					Type:    entity.FieldTypeShortText,
+				},
+			),
+
+		field.Int64("size").
+			Annotations(
+				entgql.OrderField("size"),
+				entgql.Skip(entgql.SkipMutationCreateInput, entgql.SkipMutationUpdateInput),
+				annotations.Field{
+					Caption: "Size",
+					Type:    entity.FieldTypeInteger,
+				},
+			),
+
 		field.String("content").
 			SchemaType(map[string]string{
 				dialect.MySQL:    "MEDIUMTEXT",
 				dialect.Postgres: "TEXT",
 			}).
 			Annotations(
-				entgql.OrderField("content"),
+				entsql.Skip(),
 				annotations.Field{
-					Caption:   "Content",
-					Type: entity.FieldTypeLongText,
+					Caption: "Content",
+					Type:    entity.FieldTypeLongText,
 				},
 			),
 	}
@@ -134,22 +175,22 @@ func (File) Fields() []ent.Field {
 // Edges of the File.
 func (File) Edges() []ent.Edge {
 	return []ent.Edge{
-        edge.To("created_by", User.Type).
-            Unique().
-            Annotations(
+		edge.To("created_by", User.Type).
+			Unique().
+			Annotations(
 				annotations.Edge{
 					Caption:      "Created By",
-                    RelationType: entity.RelationTypeOne,
+					RelationType: entity.RelationTypeOne,
 				},
-            ),
-        
-        edge.To("updated_by", User.Type).
-            Unique().
-            Annotations(
+			),
+
+		edge.To("updated_by", User.Type).
+			Unique().
+			Annotations(
 				annotations.Edge{
 					Caption:      "Updated By",
-                    RelationType: entity.RelationTypeOne,
+					RelationType: entity.RelationTypeOne,
 				},
-            ),
+			),
 	}
 }
