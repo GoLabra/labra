02fe9d41cec5f07c167b1e958c2210a226d20032
crembel <sintamarian.david@gmail.com>
2025-07-12 13:43:19 +0300

    fix entc

--- src/app/entc.go
+++ src/app/entc.go
@@ -49,8 +49,7 @@ func init() {
 	templateFuncMap["EntMutationFieldName"] = entMutationFieldName
 	templateFuncMap["ShouldSkip"] = utils.ShouldSkip
 	templateFuncMap["Ignore"] = func(t *gen.Type) bool {
-		fmt.Println(t.Annotations["Entity"].(map[string]any)["Owner"])
-		return t.Annotations["Entity"] != nil && t.Annotations["Entity"].(map[string]any)["Owner"] != "User"
+		return t.Annotations["Entity"] == nil || t.Annotations["Entity"].(map[string]any)["Owner"] != "User"
 	}
 
 	os.MkdirAll("./domain/repo", os.ModePerm)
