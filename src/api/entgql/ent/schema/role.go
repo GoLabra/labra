d6537c37884ea43c428b389baf3ea0246682d080
crembel <sintamarian.david@gmail.com>
2025-07-31 17:05:14 +0300

    modify created by updated by relations

--- src/api/entgql/ent/schema/role.go
+++ src/api/entgql/ent/schema/role.go
@@ -94,7 +94,7 @@ func (Role) Edges() []ent.Edge {
 				entgql.Skip(entgql.SkipMutationCreateInput, entgql.SkipMutationUpdateInput),
 				annotations.Edge{
 					Caption:      "Created By",
-					RelationType: entity.RelationTypeO2M,
+					RelationType: entity.RelationTypeOne,
 				},
 			),
 		edge.To("updated_by", User.Type).
@@ -103,7 +103,7 @@ func (Role) Edges() []ent.Edge {
 				entgql.Skip(entgql.SkipMutationCreateInput, entgql.SkipMutationUpdateInput),
 				annotations.Edge{
 					Caption:      "Updated By",
-					RelationType: entity.RelationTypeO2M,
+					RelationType: entity.RelationTypeOne,
 				},
 			),
 
